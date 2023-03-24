971c3f7a70e5f1e530feabfccee55046ae596e43
gong <ddddy0114@gmail.com>
2023-03-24 18:40:59 +0900

    delete code

--- first_app/lib/homework/week1/week1.dart
+++ first_app/lib/homework/week1/week1.dart
@@ -16,48 +16,7 @@ class MyApp extends StatelessWidget {
           title: Text("1주차 도전하기"),
         ),
         body: ListView(
-          children: [
-            SizedBox(
-              height: 80,
-              child: Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(13.0),
-                    child: Image(
-                      image: NetworkImage('https://blog.kakaocdn.net/dn/c2yJ7I/btqwXeUM6jI/a3WrMGPo9vakaDzQWepkOK/img.jpg'),
-                      width: 70,
-                      height: 70,
-                    ),
-                  ),
-                  Text(
-                    "게더",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                  ),
-                  Icon(Icons.more_horiz, size: 50,)
-                ],
-              )
-            ),
-            SizedBox(
-              height: 80,
-              child: Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(13.0),
-                    child: Image(
-                      image: NetworkImage('https://blog.kakaocdn.net/dn/c2yJ7I/btqwXeUM6jI/a3WrMGPo9vakaDzQWepkOK/img.jpg'),
-                      width: 70,
-                      height: 70,
-                    ),
-                  ),
-                  Text(
-                    "게더",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-                  ),
-                  Icon(Icons.more_horiz, size: 50,)
-                ],
-              )
-            ),
-          ],
+          
         ),
       )
     );
