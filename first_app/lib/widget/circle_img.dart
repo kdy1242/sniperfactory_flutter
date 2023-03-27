243522523708ddbc289829036eed1f44879be636
gong <ddddy0114@gmail.com>
2023-03-27 13:26:18 +0900

    [widget] CircleAvatar, ClipOval, ClipRRect

---
+++ first_app/lib/widget/circle_img.dart
@@ -0,0 +1,67 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('이테디'),
+              subtitle: Text('010-1000-2000'),
+              trailing: Icon(Icons.call),
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage('https://picsum.photos/100/100'),
+              ),
+            ),
+            ListTile(
+              title: Text('김스틴'),
+              subtitle: Text('010-9000-8000'),
+              trailing: Icon(Icons.call),
+              leading: ClipOval(
+                child: Image.network('https://picsum/50/50')
+              ),
+            ),
+            ListTile(
+              title: Text('이주노'),
+              subtitle: Text('010-3000-3000'),
+              trailing: Icon(Icons.call),
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(99),
+                child: Image.network('https://picsum.photos/100/100'),
+              ),
+            ),
+            ListTile(
+              title: Text('임헬렌'),
+              subtitle: Text('010-2000-8000'),
+              trailing: Icon(Icons.call),
+              leading: Container(
+                decoration: BoxDecoration(
+                  // borderRadius: BorderRadius.circular(99),
+                  shape: BoxShape.circle
+                ),
+                clipBehavior: Clip.antiAlias,
+                child: Image.network('https://picsum.photos/100/100'),
+              ),
+            ),
+            ListTile(
+              title: Text('염해리'),
+              subtitle: Text('010-1000-1000'),
+              trailing: Icon(Icons.call),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
